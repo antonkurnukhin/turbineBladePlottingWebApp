387f7e2444231600da5f452a87f41eef9481b584
antonkurnukhin <anton.kurnuhin@yandex.ru>
2022-06-23 18:06:06 +0300

    Update app.py
    YAML support
    Stator and rotor calculation

--- app.py
+++ app.py
@@ -1,38 +1,17 @@
+import codecs
+from pprint import pprint
 from flask import Flask, render_template, request, jsonify
+import yaml
+
 from src.blade_geometry_calculation import create_blade_section
+from src.velocity_triangle_calculation import calculate_velocity_triangle
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev'
 
-FORM_FIELDS = {
-    'blade_chord': {
-        'description': 'Хорда лопатки, [мм]',
-        'default': 28.3}, 
-    'blade_inlet_angle': {
-        'description': 'Угол входа потока, [град.]',
-        'default':30.},
-    'blade_outlet_angle': {
-        'description': 'Угол выхода потока, [град.]',
-        'default': 30.},
-    'blade_installation_angle': {
-        'description': 'Установочный угол, [град.]',
-        'default': 90.},
-    'blade_inlet_opening_angle': {
-        'description': 'Угол раскрытия передней кромки, [град.]',
-        'default': 10.},
-    'blade_outlet_opening_angle': {
-        'description': 'Угол раскрытия задней кромки, [град.]',
-        'default': 8.},
-    'leading_edge_radius': {
-        'description': 'Радиус передней кромки, [мм]',
-        'default': round(28.3*0.025, 3)},
-    'trailing_edge_radius': {
-        'description': 'Радиус задней кромки, [мм]',
-        'default': round(28.3*0.01, 3)},
-    'relative_step': {
-        'description': 'Относительный шаг турбинной решетки',
-        'default': 0.7},
-}
+
+with codecs.open('form_fields.yaml', 'r', encoding='utf8') as file: 
+    FORM_FIELDS = yaml.safe_load(file)
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -48,24 +27,54 @@ def theory():
 @app.route('/api', methods=['POST', 'GET'])
 def calculate_geometry():
     if request.method == 'POST':
-        data = {k: float(v) for k, v in request.get_json().items()}
-        calculated_data = create_blade_section(
-            blade_chord=data['blade_chord'],
-            blade_inlet_angle=data['blade_inlet_angle'],
-            blade_outlet_angle=data['blade_outlet_angle'],
-            blade_installation_angle=data['blade_installation_angle'],
-            blade_inlet_opening_angle=data['blade_inlet_opening_angle'],
-            blade_outlet_opening_angle=data['blade_outlet_opening_angle'],
-            leading_edge_radius=data['leading_edge_radius'],
-            trailing_edge_radius=data['trailing_edge_radius'],
-        )
+        data = {k: float(v) for k, v in request.get_json().items() if k != 'projectName'}
+
+        calculation_data = {
+            'stator': create_blade_section(
+                blade_chord=data['stator_blade_chord'],
+                blade_inlet_angle=data['stator_blade_inlet_angle'],
+                blade_outlet_angle=data['stator_blade_outlet_angle'],
+                blade_installation_angle=(data['stator_blade_inlet_angle']+180-data['stator_blade_outlet_angle'])/2,
+                blade_inlet_opening_angle=data['stator_blade_inlet_opening_angle'],
+                blade_outlet_opening_angle=data['stator_blade_outlet_opening_angle'],
+                leading_edge_radius=data['stator_leading_edge_radius'],
+                trailing_edge_radius=data['stator_trailing_edge_radius'],
+            ),
+            'rotor': create_blade_section(
+                blade_chord=data['rotor_blade_chord'],
+                blade_inlet_angle=data['rotor_blade_inlet_angle'],
+                blade_outlet_angle=data['rotor_blade_outlet_angle'],
+                blade_installation_angle=(data['rotor_blade_inlet_angle']+180-data['rotor_blade_outlet_angle'])/2,
+                blade_inlet_opening_angle=data['rotor_blade_inlet_opening_angle'],
+                blade_outlet_opening_angle=data['rotor_blade_outlet_opening_angle'],
+                leading_edge_radius=data['rotor_leading_edge_radius'],
+                trailing_edge_radius=data['rotor_trailing_edge_radius'],
+            )
+        }
+        velocity_triangle_data = calculate_velocity_triangle(
+            inlet_absolute_velocity=data['inlet_absolute_velocity'],
+            outlet_absolute_velocity=data['outlet_absolute_velocity'],
+            inlet_relative_velocity=data['inlet_relative_velocity'],
+            outlet_relative_velocity=data['outlet_relative_velocity'],
+            inlet_absolute_angle=data['stator_blade_inlet_angle'],
+            outlet_absolute_angle=data['stator_blade_outlet_angle'],
+            inlet_relative_angle=data['rotor_blade_inlet_angle'],
+            outlet_relative_angle=data['rotor_blade_outlet_angle'])
+
+        blade_types_to_export = ['rotor', 'stator']
+        number_of_blade = 3
+
         plotly_data = {
-            'x0': list(calculated_data['x_array']),
-            'y0': list(calculated_data['y_array']),
-            'x1': list(calculated_data['x_array']+data['blade_chord']*data['relative_step']),
-            'y1': list(calculated_data['y_array']),
-            'x2': list(calculated_data['x_array']-data['blade_chord']*data['relative_step']),
-            'y2': list(calculated_data['y_array']),
+            f'{_type}_blade_{index}': [
+                list(
+                    (calculation_data[_type]['x_array']-data[f'{_type}_blade_chord']*data[f'{_type}_relative_step'] if index==0 else \
+                        calculation_data[_type]['x_array']+data[f'{_type}_blade_chord']*data[f'{_type}_relative_step'] if index==2 else \
+                            calculation_data[_type]['x_array']) * (-1 if _type=='stator' else 1)
+                ), list(
+                    calculation_data[_type]['y_array']+(data[f'{_type}_blade_chord']/2+data['axial_gap']) if _type=='stator' else \
+                        calculation_data[_type]['y_array']-(data[f'{_type}_blade_chord']/2+data['axial_gap'])
+                )
+            ] for index in range(number_of_blade) for _type in blade_types_to_export
         }
         return jsonify(plotly_data)
     
